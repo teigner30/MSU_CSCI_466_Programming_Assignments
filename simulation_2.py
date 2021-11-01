d99acc6e6d7869adfb4a5e4944a0c5acf8ae93ce
bryndonw <bryndonw99@gmail.com>
2021-11-01 15:57:50 -0600

    renaming per assignment

---
+++ simulation_2.py
@@ -0,0 +1,60 @@
+"""
+Created on Oct 12, 2016
+
+@author: mwittie
+"""
+
+import network_2
+import link_2
+import threading
+from time import sleep
+from rprint import print
+
+
+# configuration parameters
+router_queue_size = 0  # 0 means unlimited
+simulation_time = 3  # give the network sufficient time to transfer all packets before quitting
+
+if __name__ == '__main__':
+	object_L = []  # keeps track of objects, so we can kill their threads
+	
+	# create network nodes
+	client = network_2.Host(1)
+	object_L.append(client)
+	server = network_2.Host(2)
+	object_L.append(server)
+	router_a = network_2.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+	object_L.append(router_a)
+	
+	# create a Link Layer to keep track of links between network nodes
+	link_layer = link_2.LinkLayer()
+	object_L.append(link_layer)
+	
+	# add all the links
+	# link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
+	link_layer.add_link(link_2.Link(client, 0, router_a, 0, 30))
+	link_layer.add_link(link_2.Link(router_a, 0, server, 0, 30))
+	
+	# start all the objects
+	thread_L = [threading.Thread(name=object.__str__(), target=object.run) for object in object_L]
+	for t in thread_L:
+		t.start()
+
+	ident = 0
+	# create some send events
+	message = 'there once was a man on top of a hill, he liked to jump, he liked to write with a quill, ' \
+			  'and upon his window sill, there sat a plant named bill'
+	print('sending message')
+	client.udt_send(2, ident, 1, 0, message)
+	ident += 1
+	
+	# give the network sufficient time to transfer all packets before quitting
+	sleep(simulation_time)
+	
+	# join all threads
+	for o in object_L:
+		o.stop = True
+	for t in thread_L:
+		t.join()
+	
+	print("All simulation threads joined")
